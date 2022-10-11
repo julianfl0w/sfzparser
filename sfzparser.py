d4cb2a538b247cd0a8d5acf6333ae8ba441b3fe6
Julian Loiacono <jcloiacon@gmail.com>
2022-10-11 18:44:41 -0400

    Update sfzparser.py

--- sfzparser.py
+++ sfzparser.py
@@ -4,10 +4,16 @@
 
 import math
 import re
-
+from pathlib import Path
+import os
+import sys
 from collections import OrderedDict
+import numpy as np
 from io import open
-
+import pickle
+from tqdm import tqdm
+import json
+import librosa
 
 SFZ_NOTE_LETTER_OFFSET = {'a': 9, 'b': 11, 'c': 0, 'd': 2, 'e': 4, 'f': 5, 'g': 7}
 
@@ -50,6 +56,7 @@ class SFZParser(object):
             self.parse(sfz)
 
     def parse(self, sfz):
+        section_name = ""
         sections = self.sections
         cur_section = []
         value = None
@@ -94,6 +101,204 @@ class SFZParser(object):
 
         return sections
 
+
+class SFZInstrument:
+    def __init__(
+        self,
+        samplesBasedir,
+        sfzFilename,
+        platform_simple,
+        SAMPLE_BUFFER_SIZE_SAMPLES,
+        CHANNELS,
+        SAMPLE_FREQUENCY,
+    ):
+
+        self.sfzFilename = sfzFilename
+        self.sfzFilenameBasedir = os.path.dirname(sfzFilename)
+        self.samplesBasedir = Path(samplesBasedir).resolve()
+        # see if we have already converted the wavs to necessary binary format
+
+        # check if binary file has already been exported
+        
+        binFilename = os.path.join(self.sfzFilenameBasedir, "_" + platform_simple + ".bin")
+        if os.path.exists(binFilename):
+            with open(binFilename, "rb") as f:
+                self.sampleFilename2address, self.sampleFilename2lengthSamples, self.binaryBlob = pickle.load(
+                    f
+                )
+        # otherwise create it!
+        else:
+            startAddr = 0
+            # initialize the info
+            self.sampleFilename2address = {}
+            self.sampleFilename2lengthSamples = {}
+            self.sampleFilename2channelCount = {}
+            # binary blob contains only mono files
+            # multitrack files will be broken apart and postpended
+            # ex out.wav becomes out.wav0 and out.wav1
+            self.binaryBlob = np.zeros((0), dtype=np.float32)
+
+            self.allSampleFilenames = [
+                str(s.resolve()) for s in list(Path(samplesBasedir).rglob("*.wav"))
+            ]
+            self.allSampleFilenames += [
+                str(s.resolve()) for s in list(Path(samplesBasedir).rglob("*.flac"))
+            ]
+            # self.computeShader.sampleBuffer.zeroInitialize() # NO LONGER NECESSARY
+            # load all the samples into a buffer
+            for sampleFilename in tqdm(self.allSampleFilenames):
+                #print("loading " + sampleFilename)
+                
+                y, samplerate = librosa.load(
+                    sampleFilename,
+                    sr=SAMPLE_FREQUENCY * 4,  # because shader access is 16byte
+                )
+                # addr = self.computeShader.sampleBuffer.write(y)
+                #self.binaryBlob[startAddr : startAddr + len(y)] = y
+                
+                if len(np.shape(y)) == 1:
+                    channelCount = 1
+                else:
+                    channelCount = np.shape(y)[1]
+                    
+                self.sampleFilename2channelCount[sampleFilename] = channelCount
+                for channel in range(channelCount):
+                    self.binaryBlob = np.append(self.binaryBlob, y, axis=0)
+                    sampleFilenameAndChannel = sampleFilename + "_" + str(channel)
+                    self.sampleFilename2address[sampleFilenameAndChannel] = startAddr
+                    self.sampleFilename2lengthSamples[sampleFilenameAndChannel] = len(y)
+                startAddr += len(y)
+
+            # save binary file for reloading
+            with open(binFilename, "wb+") as f:
+                pickle.dump(
+                    (
+                        self.sampleFilename2address,
+                        self.sampleFilename2lengthSamples,
+                        self.binaryBlob,
+                    ),
+                    f,
+                )
+
+        ## save entire self for reloading
+        # with open(binFilename, "wb+") as f:
+        #    pkl.dump(self.sfzInst, f)
+
+        self.loadSFZ(sfzFilename=sfzFilename)
+
+    def preprocess(self, sfzFilename, replaceDict = {}):
+        
+        # read in the template sfz
+        with open(sfzFilename, "r") as f:
+            preprocFile = f.read()
+
+        preProcessText = ""
+        for ogline in preprocFile.split("\n"):
+            ogline=ogline.strip()
+            line=ogline.split("//")[0]
+            for k, v in replaceDict.items():
+                line = line.replace(k,v)
+            # keep the comments
+            if ogline.startswith("//"):
+                preProcessText += ogline
+                
+            elif "#include" in line:
+                for includeFile in line.split("#include")[1:][::2]:
+                    includeFilename = eval(includeFile.strip())
+                    includeFilename = os.path.join(self.sfzFilenameBasedir, includeFilename)
+                    includeText = self.preprocess(includeFilename, replaceDict=replaceDict.copy())
+                    preProcessText += "\n" + includeText + "\n"
+            
+            elif line.startswith("#define"):
+                preProcessText += line
+                k = line.split(" ")[1]
+                v = "".join(line.split(" ")[2:])
+                replaceDict[k] = v
+            else:
+                preProcessText += line
+                
+            preProcessText += "\n"
+        
+        return preProcessText
+        
+    def loadSFZ(self, sfzFilename, depth=0):
+        print("loading from " + sfzFilename)
+        preProcessText = self.preprocess(sfzFilename)
+        with open("a.spz", "w+") as f:
+            f.write(preProcessText)
+            
+        sfzParser = SFZParser("a.spz")
+        # pprint.pprint(sfzParser.sections)
+
+        if depth == 0:
+            self.regions = []
+            self.globalDict = {}
+            self.masterDict = {}
+            self.groupDict = {}
+
+        for sectionName, valueDict in sfzParser.sections:
+            # print(sectionName)
+            # print(valueDict)
+
+            if sectionName == "region":
+                # add all the global items
+                for k, v in self.groupDict.items():  # GROUP
+                    if k not in valueDict.keys():
+                        valueDict[k] = v
+                for k, v in self.masterDict.items():  # MASTER
+                    if k not in valueDict.keys():
+                        valueDict[k] = v
+                for k, v in self.globalDict.items():  # GLOBAL
+                    if k not in valueDict.keys():
+                        valueDict[k] = v
+
+                # resolve sample path
+                resolved = str(
+                    Path(
+                        os.path.join(self.samplesBasedir, valueDict["sample"])
+                    ).resolve()
+                )
+                valueDict["sample"] = resolved
+
+                self.regions += [valueDict]
+
+            elif sectionName == "global":
+                self.globalDict = valueDict
+
+            elif sectionName == "master":
+                self.masterDict = valueDict
+
+            elif sectionName == "group":
+                self.groupDict = valueDict
+                # print(json.dumps(valueDict, indent=2))
+
+            elif sectionName == "control":
+                pass
+
+            elif sectionName == "comment":
+                pass
+
+            elif sectionName == "":
+                pass
+            else:
+                raise Exception("Unknown sfz header \'" + str(sectionName) + "\'")
+
+        def noteInSfzRegion(noteNo, region):
+            inRegion = True
+            if "lokey" in region.keys() and noteNo < eval(region["lokey"]):
+                inRegion = False
+            if "hikey" in region.keys() and noteNo > eval(region["hikey"]):
+                inRegion = False
+            return inRegion
+
+        # attach salient regions to their midi note
+        self.note2regions = []
+        for i in range(128):
+            thisNoteRegions = []
+            for region in self.regions:
+                if noteInSfzRegion(i, region):
+                    thisNoteRegions += [region]
+            self.note2regions += [thisNoteRegions]
 
 if __name__ == '__main__':
     import pprint
